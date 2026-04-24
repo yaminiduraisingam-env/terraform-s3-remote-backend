01656e6cbbc050b28e8d844871dd48138ec2f060
yaminiduraisingam-env <yamini.duraisingam@env0.com>
2026-04-24 15:45:35 -0400

    Update main.tf

--- bootstrap/main.tf
+++ bootstrap/main.tf
@@ -21,7 +21,6 @@ locals {
 
 resource "aws_s3_bucket" "terraform_state" {
   bucket = local.bucket_name
-  force_destroy = true
 }
 
 # Enable versioning so every state push creates a recoverable version.
