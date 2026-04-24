4d70bed24a1f0ac150534d44ef67bed6126750b8
yaminiduraisingam-env <yamini.duraisingam@env0.com>
2026-04-24 11:40:45 -0400

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -15,5 +15,10 @@
 
 terraform {
   backend "s3" {
+    bucket         = "tf-remote-backend-state-013141018419-eu-central-1"
+    key            = "infra/terraform.tfstate"
+    region         = "eu-central-1"
+    dynamodb_table = "tf-remote-backend-locks"
+    encrypt        = true
   }
 }
